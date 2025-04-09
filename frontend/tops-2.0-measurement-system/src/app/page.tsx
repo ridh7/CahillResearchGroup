7532d168af36523ddf57aecbc879766b9dfd3aec
Ridhwik Kalgaonkar <rk44@illinois.edu>
2025-04-09 09:37:43 -0500

    add heatmaps

--- frontend/tops-2.0-measurement-system/src/app/page.tsx
+++ frontend/tops-2.0-measurement-system/src/app/page.tsx
@@ -4,7 +4,7 @@ import { useState, useRef, useEffect } from "react";
 import { AnimatePresence } from "framer-motion";
 import MetadataPanel from "../components/MetadataPanel";
 import DeviceControls from "../components/DeviceControls";
-import GraphsPanel from "../components/GraphsPanel";
+import HeatMapsPanel from "../components/HeatMapsPanel";
 import OutputPanel from "../components/OutputPanel";
 import SettingsPanel from "../components/SettingsPanel";
 
@@ -551,7 +551,7 @@ export default function CalculatePage() {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth={2}
-              d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+              d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c-.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
             />
             <path
               strokeLinecap="round"
@@ -578,16 +578,10 @@ export default function CalculatePage() {
         </div>
 
         {/* Center Panel */}
-        <GraphsPanel
+        <HeatMapsPanel
           lockinData={lockinData}
           multimeterData={multimeterData}
-          lockinConnected={lockinConnected}
-          multimeterConnected={multimeterConnected}
-          resetLockin={resetLockinTrigger}
-          resetMultimeter={resetMultimeterTrigger}
-          onResetComplete={handleResetComplete}
-          lockinStartTime={lockinStartTime}
-          multimeterStartTime={multimeterStartTime}
+          stageData={stageData}
         />
 
         {/* Right Panel */}
